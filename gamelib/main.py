34d27ade8064dc183400f7c9611e1c0976889366
miniplane <jasmin.min.wagner@gmail.com>
2017-10-16 19:19:01 +0200

    draw and move square

--- gamelib/main.py
+++ gamelib/main.py
@@ -13,20 +13,53 @@ import pygame.event
 import gamelib.data as data
 
 
+
+
 def main():
     ''' entry point of the game '''
 
     pygame.init()
     screen = pygame.display.set_mode([800, 600])
 
+    square_w = 90
+    square_h = 90
+
+    square_x = 100
+    square_y = 100
+
+    color = (0, 0, 0)
+    color = (255, 255, 255)
+
+    clock = pygame.time.Clock()
+
     keep_running = True
 
+
     while keep_running:
-        event = pygame.event.wait()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_q or event.type == pygame.QUIT:
-            keep_running = False
+
+        pressed = pygame.key.get_pressed()
+
+        if pressed[pygame.K_UP]: square_y -= 3
+        if pressed[pygame.K_DOWN]: square_y += 3
+        if pressed[pygame.K_LEFT]: square_x -= 3
+        if pressed[pygame.K_RIGHT]: square_x += 3
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_q or event.type == pygame.QUIT:
+                keep_running = False
 
 
 
-    print("Hello from your game's main()")
-    print(data.load('sample.txt').read())
+        screen.fill((0, 0, 0))
+
+        pygame.draw.rect(screen, color, pygame.Rect(square_x, square_y, square_h, square_w))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+        pygame.display.flip()
+
+
+        print("Hello from your game's main()")
+        print(data.load('sample.txt').read())
+
