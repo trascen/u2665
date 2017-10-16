c092fb7eb1c4d4ce64c239a1ab36ac3c9d56630b
rin <rin@nightsky>
2017-10-16 17:55:53 +0200

    Added Python version check.

--- gamelib/depcheck.py
+++ gamelib/depcheck.py
@@ -1,9 +1,18 @@
 ''' Dependency checks '''
 
-from importlib.util import find_spec
-from itertools import filterfalse
+def check_version():
+    import sys 
+    required_version = (3,5)
+    if sys.version_info[0:2] < required_version:
+        print('This game required Python 3.5 or higher to work.')
+        return False
+    return True
+
+
 
 def check_dependencies():
+    from importlib.util import find_spec
+    from itertools import filterfalse
     '''
     Checks whether all needed 3rd party libraries are installed
     and loadable.
--- run_game.py
+++ run_game.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 from gamelib.depcheck import check_dependencies
+from gamelib.depcheck import check_version
 
-if check_dependencies():
+if check_version() and check_dependencies():
     from gamelib import main
     main.main()
