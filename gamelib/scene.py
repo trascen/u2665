95eef1e225a8618eab7c1c5d839329b9df31dc9e
trascen <s@w23.at>
2017-10-17 21:11:13 +0200

    broken collision detection

--- gamelib/scene.py
+++ gamelib/scene.py
@@ -1,6 +1,6 @@
 ''' game scenes '''
 
-from gamelib.sprite import Spritey
+from gamelib.sprite import Spritey, CollisionMap, Player
 import pygame
 
 
@@ -17,14 +17,11 @@ class Scene:
 class MainScene(Scene):
     def __init__(self):
         self.bg = Spritey('testlevel')
-        #self.canvas = pygame.Surface((320,240)).convert()
+        self.collision_map = CollisionMap('testlevel_collision_map')
         self.canvas = pygame.Surface((self.bg.surface.get_width(),self.bg.surface.get_height())).convert()
-        self.player = Spritey('player')
+        self.player = Player('player', 10, 10)
         self.square_w = self.player.surface.get_width()
         self.square_h = self.player.surface.get_height()
-
-        self.square_x = 10
-        self.square_y = 10
 
         self.color = (0, 0, 0)
         self.color = (255, 255, 255)
@@ -34,25 +31,27 @@ class MainScene(Scene):
     def update(self):
         pressed = pygame.key.get_pressed()
 
-        if pressed[pygame.K_UP] and self.jumptime < 50: 
-            self.square_y -= 2
-            self.jumptime += 1
 
-        elif self.square_y < self.canvas.get_height()-self.square_h:
-            self.square_y +=3
-            #square_y = 600-square_h
-            if self.square_y >= self.canvas.get_height()-self.square_h:
-                self.square_y = self.canvas.get_height()-self.square_h
+        if pressed[pygame.K_UP] and self.jumptime < 50: 
+            self.player.y -= 2
+            self.jumptime += 1
+        else:
+            floor_adjusted = self.collision_map.get_floor((self.player.x, self.player.y))[1] - self.square_h
+            self.player.y = min(floor_adjusted, self.player.y + 3)
+            if self.player.y == floor_adjusted:
                 self.jumptime = 0
-        if pressed[pygame.K_DOWN]: self.square_y += 3
-        if pressed[pygame.K_LEFT]: self.square_x -= 3
-        if pressed[pygame.K_RIGHT]: self.square_x += 3
+        if pressed[pygame.K_LEFT]: self.player.x -= 3
+        if pressed[pygame.K_RIGHT]: self.player.x += 3
+        if self.player.x < 0:
+            self.player.x = 0
+        if self.player.x >= self.bg.surface.get_width():
+            self.player.x = self.bg.surface.get_width()-1
 
         return self
     
     def draw(self, screen):
         #self.canvas.fill((127,127,127))
         self.canvas.blit(self.bg.surface, [0,0])
-        rect = pygame.Rect(self.square_x, self.square_y, self.square_h, self.square_w)
-        self.canvas.blit(self.player.surface, [self.square_x, self.square_y])
+        rect = pygame.Rect(self.player.x, self.player.y, self.square_h, self.square_w)
+        self.canvas.blit(self.player.surface, [self.player.x, self.player.y])
         pygame.transform.scale(self.canvas,(screen.get_width(), screen.get_height()), screen)
--- gamelib/sprite.py
+++ gamelib/sprite.py
@@ -1,8 +1,31 @@
 ''' sprite stuff '''
 
+import pygame
+from pygame import Color
 import pygame.image
+import pygame.mask
 import gamelib.data as data
 
 class Spritey:
     def __init__(self, name):
         self.surface = pygame.image.load(data.load(name + '.png')).convert()
+
+class Player(Spritey):
+    def __init__(self, name, x, y):
+        super().__init__(name)
+        self.x = x
+        self.y = y
+
+class CollisionMap:
+    def __init__(self, name):
+        self.free_color = Color(255,255,255)
+        self.surface = pygame.image.load(data.load(name + '.png'))
+        self.mask = pygame.mask.from_surface(self.surface)
+
+    def is_free(self, pos):
+        return self.surface.get_at(pos) == self.free_color
+
+    def get_floor(self, pos):
+        while self.is_free(pos):
+            pos = (pos[0], pos[1]+1)
+        return pos
